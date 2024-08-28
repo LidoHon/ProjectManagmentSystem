26fa4b1fef0786ba8fda19a23c8ac194e0775084
LidoHon <lidohon10@gmail.com>
2024-08-28 16:17:30 +0300

    first commit - project and client quearies

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const colors = require("colors");
+
+const connectDB = require("./config/db");
+const schema = require("./schema/schema");
+const port = process.env.PORT || 3000;
+const app = express();
+
+//  connect to database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+app.listen(port, () => console.log(`Server running on port ${port}`));
